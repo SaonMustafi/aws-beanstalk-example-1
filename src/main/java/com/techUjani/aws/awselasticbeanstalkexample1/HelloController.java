5cf66b14504ae633da27ae4d6bc82986a1a13da9
Saon Mustafi <saon_mustafi@yahoo.co.uk>
2019-08-01 16:20:01 +0530

    This is final commit

--- src/main/java/com/techUjani/aws/awselasticbeanstalkexample1/HelloController.java
+++ src/main/java/com/techUjani/aws/awselasticbeanstalkexample1/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping(value="/hello")
 	public String hello() {
-		return "Hello Youtube";
+		return "Hello Ujani";
 	}
 }
